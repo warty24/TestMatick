ba5c3d343f19662662595a5810274401abcd3991
warty <warty24@gmail.com>
2020-05-25 14:11:44 +0300

    Added trapeze constructor

--- src/Trapeze.java
+++ src/Trapeze.java
@@ -3,8 +3,9 @@ public class Trapeze extends Figure {
     int side1, side2, height;
     Point2D a,b,c,d;
     public Trapeze(){
-
-
+        side1 = (int) (Math.random()*Main.FIELD_SIZE/10)+1;
+        side2 = (int) (Math.random()*Main.FIELD_SIZE/10)+1;
+        height = (int) (Math.random()*Main.FIELD_SIZE/10)+1;
 
         color = new Color(randColor());
         square = (side1 + side2)*height/2;
